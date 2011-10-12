0906749cbfb944c75c8d99f35e940e0a60faa553
jhierrot@gmail.com <jhierrot@gmail.com@0dbe52e2-3855-517d-bc6d-d04104cc4623>
2011-10-12 19:00:48 +0000

    BETA 0.5

---
+++ ProdocWeb/src/java/prodocUI/forms/FMain.java
@@ -0,0 +1,131 @@
+/*
+ * OpenProdoc
+ * 
+ * See the help doc files distributed with
+ * this work for additional information regarding copyright ownership.
+ * Joaquin Hierro licenses this file to You under:
+ * 
+ * License GNU GPL v3 http://www.gnu.org/licenses/gpl.html
+ * 
+ * you may not use this file except in compliance with the License.  
+ * Unless agreed to in writing, software is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * author: Joaquin Hierro      2011
+ * 
+ */
+
+package prodocUI.forms;
+
+import html.*;
+import javax.servlet.http.HttpServletRequest;
+import prodoc.PDException;
+import prodoc.PDFolders;
+import prodocUI.servlet.About;
+import prodocUI.servlet.AddDoc;
+import prodocUI.servlet.AddDocAdv;
+import prodocUI.servlet.AddFold;
+import prodocUI.servlet.AddFoldAdv;
+import prodocUI.servlet.CheckOut;
+import prodocUI.servlet.CancelCheckOut;
+import prodocUI.servlet.CheckIn;
+import prodocUI.servlet.DelDoc;
+import prodocUI.servlet.DelFold;
+import prodocUI.servlet.ListVer;
+import prodocUI.servlet.ModDocAdv;
+import prodocUI.servlet.ModFold;
+import prodocUI.servlet.ModFoldAdv;
+import prodocUI.servlet.RefreshFold;
+import prodocUI.servlet.SExit;
+import prodocUI.servlet.SPaperBin;
+import prodocUI.servlet.SParent;
+import prodocUI.servlet.SPassChange;
+import prodocUI.servlet.SearchDoc;
+import prodocUI.servlet.SearchFold;
+
+/**
+ *
+ * @author jhierrot
+ */
+public class FMain extends FBase
+{
+//-----------------------------------------------------------------------------------------------    
+
+
+/** Creates a new instance of FBase
+ * @param Req
+ * @param pTitulo
+ * @param pCabecera
+ * @throws PDException
+ */
+public FMain(HttpServletRequest Req, String pTitulo, String pCabecera) throws PDException
+{
+super(Req, pTitulo, pCabecera);
+setStrict(true);
+AddOnLoad("init()");
+AddCSS("MainMenu.css");
+AddCSS("TestTree.css");
+AddJS("MainMenu.js");
+AddJS("Tree.js");
+AddJS("Types.js");
+//---- Menu -------------------------
+// TODO revisar permisos para componer dinámicamente menú
+String WorkArea="MainFrame";
+Menu MainMenu=new Menu();
+MenuCol MC1=new MenuCol(TT("Folders"), 0);
+MainMenu.add(MC1);
+MC1.add(new MenuItem(TT("Add"), AddFold.getUrlServlet(), WorkArea));
+MC1.add(new MenuItem(TT("Delete"), DelFold.getUrlServlet(), WorkArea));
+MC1.add(new MenuItem(TT("Update"), ModFold.getUrlServlet(), WorkArea));
+MC1.add(MenuItem.getMISeparator());
+MC1.add(new MenuItem(TT("Extended_Add"), AddFoldAdv.getUrlServlet(), WorkArea));
+MC1.add(new MenuItem(TT("Update_Extended"), ModFoldAdv.getUrlServlet(), WorkArea));
+MC1.add(new MenuItem(TT("Refresh"), RefreshFold.getUrlServlet(), WorkArea));
+MC1.add(MenuItem.getMISeparator());
+MC1.add(new MenuItem(TT("Search_Folders"), SearchFold.getUrlServlet(), WorkArea));
+MC1.add(MenuItem.getMISeparator());
+MC1.add(new MenuItem(TT("Exit"), SExit.getUrlServlet()));
+MenuCol MC2=new MenuCol(TT("Documents"), 1);
+MainMenu.add(MC2);
+MC2.add(new MenuItem(TT("Add"), AddDoc.getUrlServlet(), WorkArea));
+MC2.add(new MenuItem(TT("Delete"), DelDoc.getUrlServlet(), WorkArea));
+MC2.add(MenuItem.getMISeparator());
+MC2.add(new MenuItem(TT("Extended_Add"), AddDocAdv.getUrlServlet(), WorkArea));
+MC2.add(new MenuItem(TT("Update_Extended"), ModDocAdv.getUrlServlet(), WorkArea));
+MC2.add(new MenuItem(TT("Refresh"), RefreshFold.getUrlServlet(), WorkArea));
+MC2.add(MenuItem.getMISeparator());
+MC2.add(new MenuItem(TT("CheckOut"), CheckOut.getUrlServlet(), WorkArea));
+MC2.add(new MenuItem(TT("CheckIn"), CheckIn.getUrlServlet(), WorkArea));
+MC2.add(new MenuItem(TT("Cancel_CheckOut"), CancelCheckOut.getUrlServlet(), WorkArea));
+MC2.add(new MenuItem(TT("List_of_Versions"), ListVer.getUrlServlet(), WorkArea));
+MC2.add(MenuItem.getMISeparator());
+MC2.add(new MenuItem(TT("Search_Documents"), SearchDoc.getUrlServlet(), WorkArea));
+MenuCol MC3=new MenuCol(TT("Other_Tasks"), 2);
+MainMenu.add(MC3);
+MC3.add(new MenuItem(TT("Trash_bin"), SPaperBin.getUrlServlet(), WorkArea));
+MC3.add(new MenuItem(TT("Password_change"), SPassChange.getUrlServlet(), WorkArea));
+MenuCol MC4=new MenuCol(TT("Help"), 3);
+MainMenu.add(MC4);
+MC4.add(new MenuItem(TT("Contents"), "help/"+SParent.getLang(Req)+"/MainWin.html", "_blank"));
+MC4.add(new MenuItem(TT("About"), About.getUrlServlet(), "_blank"));
+AddHead(MainMenu);
+//---- Menu -------------------------
+Table CentralArea=new Table(3,1,1);
+CentralArea.setWidth(-100);
+CentralArea.setHeight(600);
+//CentralArea.getCelda(0,0).setWidth(-25);
+CentralArea.getCelda(0,0).setWidth(200);
+AddBody(CentralArea);
+Tree Ar=new Tree();
+Ar.add(new Branch(PDFolders.ROOTFOLDER, PDFolders.ROOTFOLDER, true));
+CentralArea.getCelda(0,0).AddElem(Ar);
+CentralArea.getCelda(0,0).setCSSId("IdTreeFrame");
+CentralArea.getCelda(1,0).setCSSClass("UDFrames");
+CentralArea.getCelda(1,0).setWidth(2);
+// CentralArea.getCelda(2,0).AddElem(new Element("<iframe src=\"RefreshDocs\" width=\"100%\" height=\"100%\" FRAMEBORDER=\"0\" name=\"MainFrame\"><p>Your browser does not support iframes.</p></iframe>"));
+CentralArea.getCelda(2,0).AddElem(new Element("<iframe src=\"RefreshDocs\" width=\"100%\" height=\"600px\" FRAMEBORDER=\"0\" name=\"MainFrame\"><p>Your browser does not support iframes.</p></iframe>"));
+}
+//----------------------------------------------------------------------------
+}
---
+++ ProdocWeb/src/java/prodocUI/forms/FMantFoldAdv.java
@@ -0,0 +1,145 @@
+/*
+ * OpenProdoc
+ * 
+ * See the help doc files distributed with
+ * this work for additional information regarding copyright ownership.
+ * Joaquin Hierro licenses this file to You under:
+ * 
+ * License GNU GPL v3 http://www.gnu.org/licenses/gpl.html
+ * 
+ * you may not use this file except in compliance with the License.  
+ * Unless agreed to in writing, software is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * author: Joaquin Hierro      2011
+ * 
+ */
+
+package prodocUI.forms;
+
+
+import html.*;
+import javax.servlet.http.HttpServletRequest;
+import prodoc.Attribute;
+import prodoc.DriverGeneric;
+import prodoc.PDException;
+import prodoc.PDFolders;
+import prodoc.Record;
+import prodocServ.ListTypeFolds;
+import prodocUI.servlet.SParent;
+
+/**
+ *
+ * @author jhierrot
+ */
+public class FMantFoldAdv extends FFormBase
+{
+FieldCombo ListTip;
+FieldCombo ListACL;
+public FieldText FoldTitle;
+
+/** Creates a new instance of FMantFoldAdv
+ * @param Req
+ * @param pMode
+ * @param pRec
+ * @param Destination 
+ * @throws PDException
+ */
+public FMantFoldAdv(HttpServletRequest Req, int pMode, Record pRec,String Destination) throws PDException
+{
+super(Req, SParent.TT(Req,"Maintenance_Folders"), pMode, pRec);
+AddJS("Types.js");
+Table BorderTab=new Table(1, 5, 1);
+BorderTab.setCSSId("BordTab");
+BorderTab.setCSSClass("FFormularios");
+BorderTab.setAlineacion(Table.CENTER);
+BorderTab.setWidth(-100);
+if (pMode==ADDMOD)
+    BorderTab.getCelda(0,0).AddElem(new Element(TT("Add_Folder")));
+else if (pMode==DELMOD)
+    BorderTab.getCelda(0,0).AddElem(new Element(TT("Delete_Folder")));
+else
+    BorderTab.getCelda(0,0).AddElem(new Element(TT("Update_Folder")));
+BorderTab.getCelda(0,0).setCSSClass("FTitle");
+BorderTab.getCelda(0,3).AddElem(OkButton);
+BorderTab.getCelda(0,3).AddElem(CancelButton);
+BorderTab.getCelda(0,4).AddElem(Status);
+BorderTab.getCelda(0,4).AddElem(Element.getEspacio2());
+BorderTab.getCelda(0,4).AddElem(HHelp);
+Table FormTab=new Table(4, 4, 0);
+FormTab.setCellPadding(5);
+FormTab.setWidth(-100);
+FormTab.setCSSClass("FFormularios");
+PDFolders TmpFold=new PDFolders(Session);
+Attribute Attr=TmpFold.getRecord().getAttr(PDFolders.fTITLE);
+FoldTitle=new FieldText(Attr.getName());
+FoldTitle.setMaxSize(Attr.getLongStr());
+FoldTitle.setCSSClass("FFormInput");
+FoldTitle.setMensStatus(TT(Attr.getDescription()));
+Attr=TmpFold.getRecord().getAttr(PDFolders.fFOLDTYPE);
+ListTip=new FieldCombo(Attr.getName());
+ListTip.setCSSClass("FFormInputCombo");
+ListTip.setMensStatus(TT(Attr.getDescription()));
+ListTip.AddOnChange("ListType(this.options[this.selectedIndex].value);");
+FillTip(ListTip, true);
+Attr=TmpFold.getRecord().getAttr(PDFolders.fACL);
+ListACL=new FieldCombo(Attr.getName());
+ListACL.setCSSClass("FFormInputCombo");
+ListACL.setMensStatus(TT(Attr.getDescription()));
+FillAcl(ListACL);
+FormTab.getCelda(0,0).setWidth(-25);
+FormTab.getCelda(0,0).setHeight(30);
+FormTab.getCelda(1,1).AddElem(new Element(TT("Folder_Title")+":"));
+FormTab.getCelda(1,1).setCSSClass("FFormulReq");
+FormTab.getCelda(2,1).AddElem(FoldTitle);
+FormTab.getCelda(1,2).AddElem(new Element(TT("Folder_Type")+":"));
+FormTab.getCelda(1,2).setCSSClass("FFormulReq");
+FormTab.getCelda(2,2).AddElem(ListTip);
+FormTab.getCelda(1,3).AddElem(new Element(TT("Folder_ACL")+":"));
+FormTab.getCelda(1,3).setCSSClass("FFormulReq");
+FormTab.getCelda(2,3).AddElem(ListACL);
+if (pMode!=ADDMOD)
+    {
+    String Typ=(String)pRec.getAttr(PDFolders.fFOLDTYPE).getValue();
+    ListTip.setValue(Typ);
+    ListTip.setActivado(false);
+    DriverGeneric PDSession=SParent.getSessOPD(Req);
+//    PDFolders F = new PDFolders(PDSession, Typ);
+//    F.LoadFull(SParent.getActFolderId(Req));
+//    FoldTitle.setValue(F.getTitle());
+//    ListACL.setValue(F.getACL());
+    FoldTitle.setValue((String)pRec.getAttr(PDFolders.fTITLE).getValue());
+    ListACL.setValue((String)pRec.getAttr(PDFolders.fACL).getValue());
+    if (pMode==DELMOD)
+        {
+        FoldTitle.setActivado(false);
+        ListACL.setActivado(false);
+        }
+//    Record Rec=F.getRecSum();
+    Element ListFields=ListTypeFolds.GenTabFields(Req, pRec, pMode);
+    BorderTab.getCelda(0,2).AddElem(ListFields);
+    }
+Form FolderForm=new Form(Destination+"?Read=1","FormVal");
+BorderTab.getCelda(0,1).AddElem(FormTab);
+FolderForm.AddElem(BorderTab);
+AddElem(FolderForm);
+}
+//-----------------------------------------------------------------------------------------------    
+@Override
+protected String getFormHelp()
+{
+switch (Mode)  
+    {
+    case ADDMOD:
+        return("AddExtFolder");
+    case DELMOD:
+        return("DelFolder");
+    case EDIMOD:
+        return("ModExtFolder");
+    }
+return("HelpIndex");
+}
+//-----------------------------------------------------------------------------------------------    
+}
