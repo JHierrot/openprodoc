32bb9f2430d2f234f3187b1069fe6da7a69bcb02
jhierrot@gmail.com <jhierrot@gmail.com@0dbe52e2-3855-517d-bc6d-d04104cc4623>
2014-10-19 18:43:16 +0000

    Publishing. first round of test. Help updates, architecture help. OrdRecord

--- trunk/ProdocSwing/src/prodocswing/forms/TCDocsReport.java
+++ trunk/ProdocSwing/src/prodocswing/forms/TCDocsReport.java
@@ -99,7 +99,7 @@ initComponents();
         LastDateTextField.setFont(MainWin.getFontDialog());
 
         GroupLabel.setFont(MainWin.getFontDialog());
-        GroupLabel.setText(MainWin.TT("Parent_Folder"));
+        GroupLabel.setText(MainWin.TT("Group"));
 
         GroupComboBox.setFont(MainWin.getFontDialog());
         GroupComboBox.setModel(GetGroups());
@@ -173,17 +173,16 @@ initComponents();
                 .addGap(18, 18, 18)
                 .addComponent(SubTypesCheckB)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(ParamLabel2)
-                            .addComponent(FirstDateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(ParamLabel3)
-                            .addComponent(LastDateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addComponent(GroupLabel))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(ParamLabel2)
+                    .addComponent(FirstDateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(ParamLabel3)
+                    .addComponent(LastDateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(GroupLabel)
                     .addComponent(GroupComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(ParamLabel4)
                     .addComponent(OPDPathTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
